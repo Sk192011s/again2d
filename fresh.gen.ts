dbd24592d9d598ebf863738acc28789d7ac5b75e
Sk192011s <soekyawwin68@gmail.com>
2025-11-25 12:54:18 +0630

    Create fresh.gen.ts

---
+++ fresh.gen.ts
@@ -0,0 +1,13 @@
+import * as $0 from "./routes/admin.tsx";
+import * as $1 from "./routes/index.tsx";
+
+const manifest = {
+  routes: {
+    "./routes/admin.tsx": $0,
+    "./routes/index.tsx": $1,
+  },
+  islands: {},
+  baseUrl: import.meta.url,
+};
+
+export default manifest;
