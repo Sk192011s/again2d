4c9871ada602bc39ccd7de3c5fe9e3ecb26b3e54
Sk192011s <soekyawwin68@gmail.com>
2025-11-25 13:36:20 +0630

    Delete twind.config.ts

--- twind.config.ts
+++
@@ -1,9 +0,0 @@
-import { Options } from "$fresh/plugins/twind.ts";
-import { defineConfig } from "twind";
-
-export default {
-  selfURL: import.meta.url,
-  ...defineConfig({
-    theme: { extend: {} },
-  }),
-} as Options;
